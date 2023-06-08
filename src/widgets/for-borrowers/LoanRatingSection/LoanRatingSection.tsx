9b97c718d6ca0ba615f3a54f1261a46b3f6220cd
Loghorrean <loghorrean74@gmail.com>
2023-06-08 17:13:47 +0300

    Fixed hero section bug with horizontal movement

--- src/widgets/for-borrowers/LoanRatingSection/LoanRatingSection.tsx
+++ src/widgets/for-borrowers/LoanRatingSection/LoanRatingSection.tsx
@@ -1,11 +1,21 @@
 import styles from "./LoanRatingSection.module.scss";
 import {Container} from "@/src/shared/ui/layout";
+import SecondaryHeading from "@/src/shared/ui/typography/Heading/decorators/SecondaryHeading";
+import {Heading} from "@/src/shared/ui/typography";
+import {HEADING_TYPE} from "@/src/shared/ui/typography/Heading/Heading";
+import GreenText from "@/src/shared/ui/typography/GreenText";
 
 const LoanRatingSection = () => {
     return (
         <section className={styles.loan_rating_section}>
             <Container>
-                
+                <SecondaryHeading>
+                    <Heading headingType={HEADING_TYPE.H2}>
+                        Чтобы рассчитать ставку займа, мы
+                        <GreenText><span>присваиваем заёмщику рейтинг.</span></GreenText> Из
+                        чего он складывается?
+                    </Heading>
+                </SecondaryHeading>
             </Container>
         </section>
     );
