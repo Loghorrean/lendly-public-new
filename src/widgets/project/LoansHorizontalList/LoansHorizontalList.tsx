f1e703b819dfe3cb61e1c739d7b14a182402140c
Loghorrean <loghorrean74@gmail.com>
2023-07-08 13:30:52 +0300

    Mock loans list

--- src/widgets/project/LoansHorizontalList/LoansHorizontalList.tsx
+++ src/widgets/project/LoansHorizontalList/LoansHorizontalList.tsx
@@ -1,9 +1,12 @@
 import styles from "./LoansHorizontalList.module.scss";
+import FinancingProjectCard from "@/src/entities/project/ui/FinancingProjectCard";
 
 const LoansHorizontalList = () => {
     return (
         <ul className={styles.loans_horizontal_list}>
-
+            <FinancingProjectCard />
+            <FinancingProjectCard />
+            <FinancingProjectCard />
         </ul>
     );
 }
