edd39c642744fa9b922c23fb953ff6b55df2ebd2
Loghorrean <loghorrean74@gmail.com>
2023-09-15 14:32:25 +0300

    Documents tab and footer query sync

--- app/(landing)/(white)/blog/[slug]/page.tsx
+++ app/(landing)/(white)/blog/[slug]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import BlogPost from "../../../../../src/widgets/post/BlogPost";
 import ReadMoreSection from "@/src/widgets/post/ReadMoreSection";
+import BlogPost from "@/src/widgets/post/BlogPost";
 
 export default function DetailedPost({ params }: { params: { slug: string } }) {
     return (
